a0cce485a38e589a96f526514941ae13aaca3aa0
Lizeth <lizethcarmonadavila@gmail.com>
2025-11-25 16:11:20 -0500

    cierre con la x

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ model = YOLO("C:\\Users\\Lizeth\\OneDrive\\Escritorio\\v6-ANPR\\runs\\detect\\tr
 
 cap = cv2.VideoCapture(0)
 
+
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
@@ -15,7 +16,7 @@ while cap.isOpened():
     annotated_frame = results[0].plot()
     cv2.imshow("YOLOv8 Detection", annotated_frame)
 
-    if cv2.waitKey(1) & 0xFF == 27:  # Press 'esc' to exit
+    if cv2.waitKey(1) & 0xFF == 27 or cv2.getWindowProperty("YOLOv8 Detection", cv2.WND_PROP_VISIBLE) < 1:  # Press 'esc' to exit
         break
 
 cap.release()
