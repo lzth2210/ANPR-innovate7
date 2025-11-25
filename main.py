34716d16d4e4a6678ed76e48c1009fadb60262e9
Lizeth <lizethcarmonadavila@gmail.com>
2025-11-25 15:24:43 -0500

    funcionamiento de la version 6.1, deteccion basica de placas sin ocr

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 from ultralytics import YOLO
-model = YOLO("yolov8n.pt")
+import cv2
 
-data_path = "C:\\Users\\Lizeth\\OneDrive\\Escritorio\\v6-ANPR\\ANPR-Innovate7-2\\data.yaml"
-results = model.train(data=data_path, epochs=15, imgsz=512)
+model = YOLO("C:\\Users\\Lizeth\\OneDrive\\Escritorio\\v6-ANPR\\runs\\detect\\train\\weights\\best.pt")
+
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    results = model(frame)
+
+    annotated_frame = results[0].plot()
+    cv2.imshow("YOLOv8 Detection", annotated_frame)
+
+    if cv2.waitKey(1) & 0xFF == 27:  # Press 'esc' to exit
+        break
+
+cap.release()
+cv2.destroyAllWindows()
